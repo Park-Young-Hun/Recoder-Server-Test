75b4554a3e1301e44620634aaaf80cd065be83de
Park-Young-Hun <55652627+Park-Young-Hun@users.noreply.github.com>
2020-08-17 02:08:04 +0900

    ejs, ajax practice(20200816).
    ejs, ajax practice(20200816).

--- recoder2020/app.js
+++ recoder2020/app.js
@@ -1,14 +1,30 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
 app.listen(3000, function() {
     console.log("Start!! express server on port 3000");
 
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
 
 // app.get('/', function(req,res) {
     
 //     res.sendFile(__dirname + "/public/main.html");
 // });
 
+app.post('/email_post', function(req, res) {
+    console.log(req.body.email);
+    //res.send("<h1>Welcome!" + req.body.email + "</hi>");
+    res.render('email.ejs', {'email' : req.body.email});
+});
+
+app.post('/ajax_send_email', function(req, res) {
+    console.log(req.body.email);
+    var responseData = {'result' : 'ok', 'email' : req.body.email}
+    res.json(responseData)
+})
